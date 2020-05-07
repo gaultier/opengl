8b6b0f583dd476f28ada8fb5ae0b32735f9d2ed7
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-05-07 10:36:25 +0200

    SDL_Quit

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include "SDL2/SDL.h"
 
 int main() {
+    atexit(SDL_Quit);
+
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
          SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Unable to initialize SDL: %s", SDL_GetError());
-         return 1;
     }
 
 }
