971e2175cfcd90e24bf6226de9c5b5655d17b294
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-05-07 10:57:15 +0200

    Add compile flags & set gl version

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "SDL2/SDL.h"
 
 int main() {
@@ -6,5 +7,10 @@ int main() {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
          SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Unable to initialize SDL: %s", SDL_GetError());
     }
+
+    // Version
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
 }
