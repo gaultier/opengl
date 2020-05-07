365de0940692745b8b4905d27da606fc3437156c
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-05-07 17:30:45 +0200

    Poll event

--- opengl_lifecycle.c
+++ opengl_lifecycle.c
@@ -74,25 +74,31 @@ static GLuint gl_scene_setup() {
 }
 
 void gl_loop(SDL_Window* window) {
-    u16 fps_desired = 60;
-    u16 frame_rate = 1000 / fps_desired;
+    /* u16 fps_desired = 60; */
+    /* u16 frame_rate = 1000 / fps_desired; */
 
-    u32 start = 0, end = 0, elapsed_time = 0;
+    /* u32 start = 0, end = 0, elapsed_time = 0; */
 
     const GLuint program_id = gl_scene_setup();
+    SDL_Event event;
 
     while (true) {
-        start = SDL_GetTicks();
+        /* start = SDL_GetTicks(); */
 
-        glClearColor(0, 0, 0, 1);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glUseProgram(program_id);
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                return;
+            }
+            glClearColor(0, 0, 0, 1);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            glUseProgram(program_id);
 
-        SDL_GL_SwapWindow(window);
+            SDL_GL_SwapWindow(window);
+        }
+        /* end = SDL_GetTicks(); */
+        /* elapsed_time = end - start; */
 
-        end = SDL_GetTicks();
-        elapsed_time = end - start;
-
-        if (elapsed_time < frame_rate) SDL_Delay(frame_rate - elapsed_time);
+        /* if (elapsed_time < frame_rate) SDL_Delay(frame_rate - elapsed_time);
+         */
     }
 }
