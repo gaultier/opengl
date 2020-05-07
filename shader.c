a7d05ebb50aa31a4ece3991af1edfe059b2afe97
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-05-07 15:41:30 +0200

    read shader files

--- shader.c
+++ shader.c
@@ -1,7 +1,11 @@
 #include "shader.h"
 
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#include "file.h"
 #include "malloc.h"
 #include "types.h"
 
@@ -12,9 +16,25 @@ GLuint shader_load(const char vertex_file_path[],
 
     const usize vertex_shader_src_capacity = 5000;
     u8* vertex_shader_src = ogl_malloc(vertex_shader_src_capacity);
+    usize vertex_shader_src_len = 0;
+
+    if (file_read("vertex_shader.glsl", vertex_shader_src,
+                  vertex_shader_src_capacity, &vertex_shader_src_len) > 1) {
+        fprintf(stderr, "Could not open file `%s`: %s", "vertex_shader.glsl",
+                strerror(errno));
+        exit(errno);
+    }
 
     const usize fragment_shader_src_capacity = 5000;
     u8* fragment_shader_src = ogl_malloc(fragment_shader_src_capacity);
+    usize fragment_shader_src_len = 0;
+
+    if (file_read("fragment_shader.glsl", fragment_shader_src,
+                  fragment_shader_src_capacity, &fragment_shader_src_len) > 1) {
+        fprintf(stderr, "Could not open file `%s`: %s", "fragment_shader.glsl",
+                strerror(errno));
+        exit(errno);
+    }
 
     return 0;
 }
