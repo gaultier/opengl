d62b73041917f0f1814577b4eca72497d80b426b
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-05-07 11:06:42 +0200

    Open window

--- main.c
+++ main.c
@@ -1,16 +1,42 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_video.h>
+#include <stdbool.h>
 #include <stdlib.h>
-#include "SDL2/SDL.h"
 
 int main() {
     atexit(SDL_Quit);
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Unable to initialize SDL: %s", SDL_GetError());
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Unable to initialize SDL: %s",
+                     SDL_GetError());
     }
 
     // Version
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
+                        SDL_GL_CONTEXT_PROFILE_CORE);
 
+    // Double Buffer
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+    _Bool fullscreen_ = false;
+    Uint32 flags = SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE;
+    if (fullscreen_) {
+        flags |= SDL_WINDOW_MAXIMIZED;
+    }
+
+    const char window_title[] = "hello";
+    const int window_width = 800;
+    const int window_height = 600;
+    SDL_Window* window = SDL_CreateWindow(window_title, SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED, window_width,
+                                          window_height, flags);
+
+    if (!window) {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Unable to create window: %s",
+                     SDL_GetError());
+        SDL_Quit();
+    }
 }
